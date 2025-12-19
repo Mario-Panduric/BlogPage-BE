76cc44c916a5be066a125fdc90845dbd93dbe004
mario <mariopanduric123@hotmail.com>
2025-12-19 19:22:41 +0100

    Added image wallpaper for every blog

--- WebApplication5/DTOs/GetPostDto.cs
+++ WebApplication5/DTOs/GetPostDto.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Content { get; set; }
+        public required byte[] Image { get; set; }
         public required int userId { get; set; }
         DateTime CreatedAt { get; set; }
 
